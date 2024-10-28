bfa20d9d2787e9a7956e35d30df6b41278d1c904
Grëg Häris <d.gregharis@gmail.com>
2024-10-28 13:36:10 +0100

    create the components

---
+++ src/app/components/SearchBar.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+const SearchBar = ({ children }: Props) => {
+  return <div className="searchBar">{children}</div>;
+};
+
+export default SearchBar;
