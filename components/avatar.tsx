493d8a0d1250571c1227c11104556cfce0893d49
Grëg Häris <d.gregharis@gmail.com>
2024-12-01 09:40:45 +0100

    fix: Refactor and clean up code

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,61 +1,49 @@
-import { forwardRef, HTMLAttributes } from 'react';
-import { cva, type VariantProps } from 'class-variance-authority';
+'use client';
+
+import * as AvatarPrimitive from '@radix-ui/react-avatar';
 import { cn } from '@/lib/utils';
+import { forwardRef, ComponentPropsWithoutRef } from 'react';
 
-const alertVariants = cva(
-  'relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground',
-  {
-    variants: {
-      variant: {
-        default: 'bg-background text-foreground',
-        destructive:
-          'border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive',
-      },
-    },
-    defaultVariants: {
-      variant: 'default',
-    },
-  },
-);
-
-interface AlertProps
-  extends HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof alertVariants> {}
-
-const Alert = forwardRef<HTMLDivElement, AlertProps>(
-  ({ className, variant, ...props }, ref) => (
-    <div
-      ref={ref}
-      role="alert"
-      className={cn(alertVariants({ variant }), className)}
-      {...props}
-    />
-  ),
-);
-Alert.displayName = 'Alert';
-
-const AlertTitle = forwardRef<
-  HTMLParagraphElement,
-  HTMLAttributes<HTMLHeadingElement>
+const Avatar = forwardRef<
+  HTMLSpanElement,
+  ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
 >(({ className, ...props }, ref) => (
-  <h5
+  <AvatarPrimitive.Root
     ref={ref}
-    className={cn('mb-1 font-medium leading-none tracking-tight', className)}
+    className={cn(
+      'relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full',
+      className,
+    )}
     {...props}
   />
 ));
-AlertTitle.displayName = 'AlertTitle';
+Avatar.displayName = AvatarPrimitive.Root.displayName;
 
-const AlertDescription = forwardRef<
-  HTMLParagraphElement,
-  HTMLAttributes<HTMLParagraphElement>
+const AvatarImage = forwardRef<
+  HTMLImageElement,
+  ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
 >(({ className, ...props }, ref) => (
-  <div
+  <AvatarPrimitive.Image
     ref={ref}
-    className={cn('text-sm [&_p]:leading-relaxed', className)}
+    className={cn('aspect-square h-full w-full', className)}
     {...props}
   />
 ));
-AlertDescription.displayName = 'AlertDescription';
+AvatarImage.displayName = AvatarPrimitive.Image.displayName;
 
-export { Alert, AlertTitle, AlertDescription };
+const AvatarFallback = forwardRef<
+  HTMLSpanElement,
+  ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Fallback
+    ref={ref}
+    className={cn(
+      'flex h-full w-full items-center justify-center rounded-full bg-muted',
+      className,
+    )}
+    {...props}
+  />
+));
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
+
+export { Avatar, AvatarImage, AvatarFallback };
