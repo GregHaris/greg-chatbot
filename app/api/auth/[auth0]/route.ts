850b176e3433ff30e2cecc6e297b00200c327adc
Grëg Häris <d.gregharis@gmail.com>
2024-12-04 08:45:06 +0100

    fix errors

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,3 +1,8 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, HandlerError } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+  onError: (req, res, error: HandlerError) => {
+    console.error(error);
+    res.status(error.status || 500).end(error.message);
+  },
+});
