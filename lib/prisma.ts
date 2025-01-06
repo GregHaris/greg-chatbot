47c205de8408569774d5c9e1f662de8c5bb99092
Grëg Häris <d.gregharis@gmail.com>
2025-01-06 15:57:33 +0100

    fix: resolve prisma error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 
-// Extend the globalThis interface to include the prisma property
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
-
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
+  const globalWithPrisma = global as typeof globalThis & {
+    prisma?: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
   }
-  prisma = global.prisma;
+  prisma = globalWithPrisma.prisma;
 }
 
 export default prisma;
